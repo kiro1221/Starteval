32780ca0bfe7fd1ec63c4f4fe6dab57993e1100c
kerolos Aziz <85971293+kiro1221@users.noreply.github.com>
2024-12-01 21:06:19 -0700

    Update environment.rkt

--- environment.rkt
+++ environment.rkt
@@ -1,16 +1,14 @@
 #lang racket
 
-(provide lookup extend-env empty-env)
+(provide empty-env lookup extend-env)
 
-(define empty-env '()) ;; empty env
+(define empty-env '()) ;; Empty environment
 
-(define (lookup var env) ;; Lookup variable 
+(define (lookup var env)
   (cond
     [(null? env) (error "Unbound variable: " var)]
     [(equal? var (car (car env))) (cdr (car env))]
     [else (lookup var (cdr env))]))
 
-;; Extend the environment with new bindings
-;; bindings: list of (var . value) pairs
 (define (extend-env bindings env)
-  (append bindings env))
+  (append bindings env)) ;; Add new bindings
