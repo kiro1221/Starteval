938d8eb30f2f781f7c9e9d311722e8f985ee572d
Jenil Jani <123282844+jeniljani410@users.noreply.github.com>
2024-12-04 00:16:07 -0700

    Nothing

--- environment.rkt
+++ environment.rkt
@@ -1,14 +1,1 @@
-#lang racket
-
-(provide empty-env lookup extend-env)
-
-(define empty-env '()) ;; Empty environment
-
-(define (lookup var env)
-  (cond
-    [(null? env) (error "Unbound variable: " var)]
-    [(equal? var (car (car env))) (cdr (car env))]
-    [else (lookup var (cdr env))]))
-
-(define (extend-env bindings env)
-  (append bindings env)) ;; Add new bindings
+.
